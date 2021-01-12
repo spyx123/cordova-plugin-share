85c5d354a08d657d08068e8ebd862bdefc60cfc1
spyx123 <66248814+spyx123@users.noreply.github.com>
2021-01-12 10:31:29 +0300

    Update Share.java

--- src/android/Share.java
+++ src/android/Share.java
@@ -6,6 +6,7 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import java.io.File;
 
 import android.content.Intent;
 
